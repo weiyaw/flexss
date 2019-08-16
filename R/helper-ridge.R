304ca02c0e456ae2ace421984d04b9f7b5481696
Kenyon Ng <work@kenyon.xyz>
2019-08-16 20:56:56 +0800

    finalise bayes_ridge_sub_v2, functions movement
    1. bayes_ridge_sub_v2 now works
    2. move some functions from 'helper-ridge' to 'main-ridge'.

--- R/helper-ridge.R
+++ R/helper-ridge.R
@@ -42,105 +42,6 @@
    }
 }
 
-## initialise theta with a penalised LS estimate, and delta with rnorms with small sd
-initialise_with_pls <- function(init, n_terms, grp, pls) {
-
-    ## init: NULL or list(pop, sub); any of the element can be NULL
-    n_pops <- length(unique(grp$pop))
-    n_subs <- length(unique(grp$sub))
-    if (is.null(init$pop)) {
-        init$pop <- matrix(pls, n_terms, n_pops, dimnames = list(NULL, levels(grp$pop)))
-    } else {
-        if (dim(init$sub) == c(n_terms, n_pops)) {
-            init$pop <- as.matrix(init$sub)
-            cat("Population initial values supplied.\n")
-        } else {
-            stop("Invalid dimension of population initial values.")
-        }
-    }
-    if (is.null(init$sub)) {
-        init$sub <- matrix(rnorm(n_terms * n_subs) * 0.01, n_terms, n_subs,
-                           dimnames = list(NULL, levels(grp$sub)))
-    } else {
-        if (dim(init$sub) == c(n_terms, n_subs)) {
-            init$sub <- as.matrix(init$sub)
-            cat("Subjects initial values supplied.\n")
-            if (is.null(colnames(init$sub))) {
-                colnames(init$sub) <- levels(grp$sub)
-            } else {
-                init$sub <- init$sub[, levels(grp$sub)]
-            }
-        } else {
-            stop("Invalid dimension of subject initial values.")
-        }
-    }
-    init
-}
-
-
-## calculates unnormalised log posterior for the subject specific model
-## vector of coef_pop
-## matrix of coef_sub
-## list of prec: pop, sub1, sub2, eps
-## list of contrib: pop, sub
-## list of para: xKmat, rank_K, dim_sub1, y
-## list of prior hyperparameters: ig_a, ig_b, iw_v, iw_lambda
-logprior_sub <- function(coef_pop, coef_sub, prec, para, prior) {
-
-    check_names <- prod(c("pop", "sub1", "sub2", "eps") %in% names(prec),
-                        c("xKmat", "rank_K", "dim_sub1") %in% names(para),
-                        c("ig_a", "ig_b", "iw_v", "iw_lambda") %in% names(prior),
-                        c("pop", "sub2", "eps") %in% names(prior$ig_a),
-                        c("pop", "sub2", "eps") %in% names(prior$ig_b))
-
-    if (check_names == 0) {
-        stop("Missing elements in the list.")
-    }
-
-    xKmat <- para$xKmat
-    rank_K <- para$rank_K
-    dim_sub1 <- para$dim_sub1
-    ig_a <- prior$ig_a
-    ig_b <- prior$ig_b
-    iw_v <- prior$iw_v
-    iw_lambda <- prior$iw_lambda          # assuming symmetric
-
-    prec_sub <- block_diag(prec$sub1, diag(prec$sub2, NROW(coef_sub) - dim_sub1))
-
-    ## inverse gamma
-    lp_prec_pop <- (ig_a$pop + 1) * log(prec$pop) - ig_b$pop * prec$pop
-    lp_prec_sub2 <- (ig_a$sub2 + 1) * log(prec$sub2) - ig_b$sub2 * prec$sub2
-    lp_prec_eps <- (ig_a$eps + 1) * log(prec$eps) - ig_b$eps * prec$eps
-
-    ## inverse Wishart
-    lp_prec_sub1 <- 0.5 * (iw_v + dim_sub1 + 1) * determinant(prec$sub1)$modulus -
-       0.5 * sum(diag(iw_lambda %*% prec$sub1))
-
-    ## Gaussian
-    lp_pop <- 0.5 * rank_K * log(prec$pop) -
-        0.5 * prec$pop * crossprod(coef_pop, xKmat %*% coef_pop)
-    lp_sub <- NCOL(coef_sub) / 2 * determinant(prec_sub)$modulus -
-        0.5 * sum(apply(coef_sub, 2, function(x) crossprod(x, prec_sub %*% x)))
-
-    lp_prec_pop + lp_prec_sub1 + lp_prec_sub2 + lp_prec_eps + lp_pop + lp_sub
-}
-
-## calculates unnormalised log-likelihood for the subject specific model
-## list of prec: eps
-## resids vector
-## n_samples
-loglike_sub <- function(prec, resids, n_samples) {
-    check_names <- prod(c("eps") %in% names(prec))
-
-    if (check_names == 0) {
-        stop("Missing elements in the list.")
-    }
-
-    n_samples / 2 * log(prec$eps) - 0.5 * prec$eps * crossprod(resids)
-}
-
-
-
 
 ## initialise theta with constraint, and delta with tnorms with small sd
 initialise_with_Amat <- function(init, n_terms, grp, Amat) {
@@ -196,107 +97,22 @@ initialise_with_Amat <- function(init, n_terms, grp, Amat) {
     init
 }
 
-check_grp <- function(grp) {
 
-    ## check grp field names
-    if (all(c("pop", "sub") %in% names(grp))) {
-        stop("Missing grp variables.")
-    }
-
-    ## check grp lengths
-    if (length(grp$pop) != length(grp$sub)) {
-        stop("Unequal length of grp$pop and grp$sub")
-    }
-
-    ## sub factor must be nested within pop factor
-    sublvl_in_pop <- tapply(grp$sub, grp$pop, unique, simplify = FALSE)
-
-    if (length(unlist(sublvl_in_pop)) > length(unique(grp$sub))) {
-        stop("Each subject can only appear in one population.")
-    }
-
-    if (all(lapply(grp, is.factor))) {
-        lapply(grp, droplevels)
-    } else {
-        lapply(grp, factor)
-    }
-}
-
-check_Bmat <- function(Bmat, Kmat) {
-
-    ## check grp field names
-    if (all(c("pop", "sub") %in% names(Bmat))) {
-        stop("Missing Bmat variables.")
-    }
-
-    ## check Bmat lengths
-    if (NROW(Bmat$pop) != NROW(Bmat$sub)) {
-        stop("Unequal length of Bmat$pop and Bmat$sub.")
-    }
-
-    ## check col of Bmat
-    if (NCOL(Bmat$pop) != NCOL(Kmat)) {
-        stop("Inconsistant columns of Bmat and Kmat.")
-    }
-    Bmat
-}
-
-check_prior <- function(prior, dim_sub1) {
-
-    ## build a prior for covariance if not give
-    if (is.null(prior)) {
-        prior <- list(ig_a = list(pop = 0.001, sub2 = 0.001, eps = 0.001),
-                      ig_b = list(pop = 0.001, sub2 = 0.001, eps = 0.001),
-                      iw_v = dim_sub1 + 1,
-                      iw_lambda = diag(dim_sub1)) # assuming symmetry
-    }
-
-    ## check prior field name
-    check_names <- prod(c("ig_a", "ig_b", "iw_v", "iw_lambda") %in% names(prior),
-                        c("pop", "sub2", "eps") %in% names(prior$ig_a),
-                        c("pop", "sub2", "eps") %in% names(prior$ig_b))
-    if (check_names == 0) {
-        stop("Missing prior hyperparameters.")
-    }
-    prior
-}
-
-calc_xB <- function(Bmat, idx_sub, para) {
-
-    ## crossproduct of Bmats (pop x pop, sub x pop, sub x sub)
-    n_subs <- para$n_subs
+initialise_samples <- function(para, size, dim_sub1, grp) {
     n_terms_pop <- para$n_terms_pop
     n_terms_sub <- para$n_terms_sub
-
-    xB_pop <- array(NA, c(n_terms_pop, n_terms_pop, n_subs),
-                    list(NULL, NULL, names(idx_sub)))
-    xB_subpop <- array(NA, c(n_terms_sub, n_terms_pop, n_subs),
-                       list(NULL, NULL, names(idx_sub)))
-    xB_sub <- array(NA, c(n_terms_sub, n_terms_sub, n_subs),
-                    list(NULL, NULL, names(idx_sub)))
-
-    for (i in names(idx_sub)) {
-        xB_pop[, , i] <- crossprod(Bmat$sub[idx_sub[[i]], ])
-        xB_subpop[, , i] <- crossprod(Bmat$sub[idx_sub[[i]], ], Bmat$pop[idx_sub[[i]], ])
-        xB_sub[, , i] <- crossprod(Bmat$pop[idx_sub[[i]], ])
-    }
-    list(pop = xB_pop, subpop = xB_subpop, sub = xB_sub)
-}
-
-calc_Bxy <- function(y, Bmat, idx_sub, para) {
-
-    n_terms_sub <- para$n_terms_sub
     n_subs <- para$n_subs
-
-    ## crossproduct of Bmat and y (Bmat_pop x y, Bmat_sub x y)
-    Bxy_pop <- matrix(NA, n_terms_sub, n_subs, dimnames = list(NULL, names(idx_sub)))
-    Bxy_sub <- matrix(NA, n_terms_sub, n_subs, dimnames = list(NULL, names(idx_sub)))
-
-    for (i in names(idx_sub)) {
-        Bxy_pop[, i] <- crossprod(Bmat$pop[idx_sub[[i]], ], y[idx_sub[[i]]])
-        Bxy_sub[, i] <- crossprod(Bmat$sub[idx_sub[[i]], ], y[idx_sub[[i]]])
-    }
-    list(pop = Bxy_pop, sub = Bxy_sub)
+    n_pops <- para$n_pops
+    list(population = array(NA, c(n_terms_pop, n_pops, size),
+                            dimnames = list(NULL, levels(grp$pop))),
+         subjects = array(NA, c(n_terms_sub, n_subs, size),
+                          dimnames = list(NULL, levels(grp$sub))),
+         precision = list(pop = rep(NA, size),
+                          sub1 = array(NA, c(dim_sub1, dim_sub1, size)),
+                          sub2 = rep(NA, size),
+                          eps = rep(NA, size)),
+         lp = rep(NA, size),
+         ll = rep(NA, size))
 }
 
 
--- R/main-ridge.R
+++ R/main-ridge.R
@@ -1,3 +1,207 @@
+## initialise theta with a penalised LS estimate, and delta with rnorms with small sd
+initialise_with_pls <- function(init, para, grp, pls) {
+
+    ## init: NULL or list(pop, sub); any of the element can be NULL
+    n_terms_pop <- para$n_terms_pop
+    n_terms_sub <- para$n_terms_sub
+    n_pops <- para$n_pops
+    n_subs <- para$n_subs
+
+    if (is.null(init$pop)) {
+        init$pop <- matrix(pls, n_terms_pop, n_pops,
+                           dimnames = list(NULL, levels(grp$pop)))
+    } else {
+        if (all(dim(init$pop) == c(n_terms_pop, n_pops))) {
+            init$pop <- as.matrix(init$pop)
+            cat("Population initial values supplied.\n")
+        } else {
+            stop("Invalid dimension of population initial values.")
+        }
+    }
+    if (is.null(init$sub)) {
+        init$sub <- matrix(rnorm(n_terms_sub * n_subs) * 0.01, n_terms_sub, n_subs,
+                           dimnames = list(NULL, levels(grp$sub)))
+    } else {
+        if (all(dim(init$sub) == c(n_terms_sub, n_subs))) {
+            init$sub <- as.matrix(init$sub)
+            cat("Subjects initial values supplied.\n")
+            if (is.null(colnames(init$sub))) {
+                colnames(init$sub) <- levels(grp$sub)
+            } else {
+                init$sub <- init$sub[, levels(grp$sub)]
+            }
+        } else {
+            stop("Invalid dimension of subject initial values.")
+        }
+    }
+    init
+}
+
+## calculates unnormalised log posterior for the subject specific model
+## vector of coef_pop
+## matrix of coef_sub
+## list of prec: pop, sub1, sub2, eps
+## list of contrib: pop, sub
+## list of para: xKmat, rank_K, dim_sub1, y
+## list of prior hyperparameters: ig_a, ig_b, iw_v, iw_lambda
+logprior_sub <- function(coef_pop, coef_sub, prec, para, prior) {
+
+    check_names <- prod(c("pop", "sub1", "sub2", "eps") %in% names(prec),
+                        c("xKmat", "rank_K", "dim_sub1") %in% names(para),
+                        c("ig_a", "ig_b", "iw_v", "iw_lambda") %in% names(prior),
+                        c("pop", "sub2", "eps") %in% names(prior$ig_a),
+                        c("pop", "sub2", "eps") %in% names(prior$ig_b))
+
+    if (check_names == 0) {
+        stop("Missing elements in the list.")
+    }
+
+    xKmat <- para$xKmat
+    rank_K <- para$rank_K
+    dim_sub1 <- para$dim_sub1
+    ig_a <- prior$ig_a
+    ig_b <- prior$ig_b
+    iw_v <- prior$iw_v
+    iw_lambda <- prior$iw_lambda          # assuming symmetric
+
+    prec_sub <- block_diag(prec$sub1, diag(prec$sub2, NROW(coef_sub) - dim_sub1))
+
+    ## inverse gamma
+    lp_prec_pop <- (ig_a$pop + 1) * log(prec$pop) - ig_b$pop * prec$pop
+    lp_prec_sub2 <- (ig_a$sub2 + 1) * log(prec$sub2) - ig_b$sub2 * prec$sub2
+    lp_prec_eps <- (ig_a$eps + 1) * log(prec$eps) - ig_b$eps * prec$eps
+
+    ## inverse Wishart
+    lp_prec_sub1 <- 0.5 * (iw_v + dim_sub1 + 1) * determinant(prec$sub1)$modulus -
+       0.5 * sum(diag(iw_lambda %*% prec$sub1))
+
+    ## Gaussian
+    lp_pop <- 0.5 * rank_K * log(prec$pop) -
+        0.5 * prec$pop * crossprod(coef_pop, xKmat %*% coef_pop)
+    lp_sub <- NCOL(coef_sub) / 2 * determinant(prec_sub)$modulus -
+        0.5 * sum(apply(coef_sub, 2, function(x) crossprod(x, prec_sub %*% x)))
+
+    lp_prec_pop + lp_prec_sub1 + lp_prec_sub2 + lp_prec_eps + lp_pop + lp_sub
+}
+
+## calculates unnormalised log-likelihood for the subject specific model
+## list of prec: eps
+## resids vector
+## n_samples
+loglike_sub <- function(prec, resids, n_samples) {
+    check_names <- prod(c("eps") %in% names(prec))
+
+    if (check_names == 0) {
+        stop("Missing elements in the list.")
+    }
+
+    n_samples / 2 * log(prec$eps) - 0.5 * prec$eps * crossprod(resids)
+}
+
+check_grp <- function(grp) {
+
+    ## check grp field names
+    if (!all(c("pop", "sub") %in% names(grp))) {
+        stop("Missing grp variables.")
+    }
+
+    ## check grp lengths
+    if (length(grp$pop) != length(grp$sub)) {
+        stop("Unequal length of grp$pop and grp$sub")
+    }
+
+    ## sub factor must be nested within pop factor
+    sublvl_in_pop <- tapply(grp$sub, grp$pop, unique, simplify = FALSE)
+
+    if (length(unlist(sublvl_in_pop)) > length(unique(grp$sub))) {
+        stop("Each subject can only appear in one population.")
+    }
+
+    grp <- lapply(grp, factor)
+    lapply(grp, droplevels)
+
+}
+
+check_Bmat <- function(Bmat, Kmat) {
+
+    ## check grp field names
+    if (!all(c("pop", "sub") %in% names(Bmat))) {
+        stop("Missing Bmat variables.")
+    }
+
+    ## check Bmat lengths
+    if (NROW(Bmat$pop) != NROW(Bmat$sub)) {
+        stop("Unequal length of Bmat$pop and Bmat$sub.")
+    }
+
+    ## check col of Bmat
+    if (NCOL(Bmat$pop) != NCOL(Kmat)) {
+        stop("Inconsistant columns of Bmat and Kmat.")
+    }
+    Bmat
+}
+
+check_prior <- function(prior, dim_sub1) {
+
+    ## build a prior for covariance if not give
+    if (is.null(prior)) {
+        prior <- list(ig_a = list(pop = 0.001, sub2 = 0.001, eps = 0.001),
+                      ig_b = list(pop = 0.001, sub2 = 0.001, eps = 0.001),
+                      iw_v = dim_sub1 + 1,
+                      iw_lambda = diag(dim_sub1)) # assuming symmetry
+    }
+
+    ## check prior field name
+    check_names <- prod(c("ig_a", "ig_b", "iw_v", "iw_lambda") %in% names(prior),
+                        c("pop", "sub2", "eps") %in% names(prior$ig_a),
+                        c("pop", "sub2", "eps") %in% names(prior$ig_b))
+    if (check_names == 0) {
+        stop("Missing prior hyperparameters.")
+    }
+    prior
+}
+
+calc_xB <- function(Bmat, idx_sub, para) {
+
+    ## crossproduct of Bmats (pop x pop, sub x pop, sub x sub)
+    n_subs <- para$n_subs
+    n_terms_pop <- para$n_terms_pop
+    n_terms_sub <- para$n_terms_sub
+
+    xB_pop <- array(NA, c(n_terms_pop, n_terms_pop, n_subs),
+                    list(NULL, NULL, names(idx_sub)))
+    xB_subpop <- array(NA, c(n_terms_sub, n_terms_pop, n_subs),
+                       list(NULL, NULL, names(idx_sub)))
+    xB_sub <- array(NA, c(n_terms_sub, n_terms_sub, n_subs),
+                    list(NULL, NULL, names(idx_sub)))
+
+    for (i in names(idx_sub)) {
+        xB_pop[, , i] <- crossprod(Bmat$pop[idx_sub[[i]], ])
+        xB_subpop[, , i] <- crossprod(Bmat$sub[idx_sub[[i]], ], Bmat$pop[idx_sub[[i]], ])
+        xB_sub[, , i] <- crossprod(Bmat$sub[idx_sub[[i]], ])
+    }
+    list(pop = xB_pop, subpop = xB_subpop, sub = xB_sub)
+}
+
+calc_Bxy <- function(y, Bmat, idx_sub, para) {
+
+    n_terms_pop <- para$n_terms_pop
+    n_terms_sub <- para$n_terms_sub
+    n_subs <- para$n_subs
+
+    ## crossproduct of Bmat and y (Bmat_pop x y, Bmat_sub x y)
+    Bxy_pop <- matrix(NA, n_terms_pop, n_subs, dimnames = list(NULL, names(idx_sub)))
+    Bxy_sub <- matrix(NA, n_terms_sub, n_subs, dimnames = list(NULL, names(idx_sub)))
+
+    for (i in names(idx_sub)) {
+        Bxy_pop[, i] <- crossprod(Bmat$pop[idx_sub[[i]], ], y[idx_sub[[i]]])
+        Bxy_sub[, i] <- crossprod(Bmat$sub[idx_sub[[i]], ], y[idx_sub[[i]]])
+    }
+    list(pop = Bxy_pop, sub = Bxy_sub)
+}
+
+
+
 
 ## update precision
 update_prec <- function(coef, resids, para, prior) {
@@ -11,8 +215,9 @@ update_prec <- function(coef, resids, para, prior) {
     Kmat <- para$Kmat
     rank_K <- para$rank_K
     n_samples <- para$n_samples
+    n_pops <- para$n_pops
     n_subs <- para$n_subs
-    n_terms <- para$n_terms
+    n_terms_sub <- para$n_terms_sub
     dim_sub1 <- para$dim_sub1
 
     ig_a <- prior$ig_a                  # a list
@@ -28,8 +233,8 @@ update_prec <- function(coef, resids, para, prior) {
     prec$eps <- rgamma(1, shape = shape_eps, rate = rate_eps)
 
     ## update sigma^2_theta
-    shape_pop <- 0.5 * rank_K + ig_a$pop
-    rate_pop <- 0.5 * crossprod(Kmat %*% coef$pop) + ig_b$pop
+    shape_pop <- 0.5 * n_pops * rank_K + ig_a$pop
+    rate_pop <- 0.5 * sum((Kmat %*% coef$pop)^2) + ig_b$pop
     prec$pop <- rgamma(1, shape = shape_pop, rate = rate_pop)
 
     ## update Sigma_dev1
@@ -39,8 +244,8 @@ update_prec <- function(coef, resids, para, prior) {
     prec$sub1 <- rWishart(1, df = df_sub1, Sigma = inv_scale_sub1)[, , 1]
 
     ## update sigma^2_dev2
-    shape_sub2 <- 0.5 * n_subs * (n_terms - dim_sub1) + ig_a$sub2
-    rate_sub2 <- 0.5 * crossprod(c(coef$sub[-(1:dim_sub1), ])) + ig_b$sub2
+    shape_sub2 <- 0.5 * n_subs * (n_terms_sub - dim_sub1) + ig_a$sub2
+    rate_sub2 <- 0.5 * sum(coef$sub[-(1:dim_sub1), ]^2) + ig_b$sub2
     prec$sub2 <- rgamma(1, shape = shape_sub2, rate = rate_sub2)
 
     prec
@@ -49,14 +254,11 @@ update_prec <- function(coef, resids, para, prior) {
 ## update theta_l
 update_theta_l <- function(xB_l, Bxy_l, xKmat, kprec, para) {
 
-    if (dim(xB_l)[3] != NCOL(Bxy_l)) {
-        stop("Inconsistent dimensions of xB_l and Bxy_l.")
-    }
     n_subs_in_pop <- NCOL(Bxy_l)
     n_terms_pop <- para$n_terms_pop
 
     ## xB_l (list of arrays of relevant xB)
-    if (all(c("pop", "sub", "subpop")) %in% names(xB_l)) {
+    if (!all(c("pop", "sub", "subpop") %in% names(xB_l))) {
         stop("Missing xB_l variables.")
     }
     xB_sub_l <- xB_l$sub
@@ -64,7 +266,7 @@ update_theta_l <- function(xB_l, Bxy_l, xKmat, kprec, para) {
     xB_subpop_l <- xB_l$subpop
 
     ## Bxy_l (list of matrices of relevant Bxy)
-    if (all(c("pop", "sub")) %in% names(Bxy_l)) {
+    if (!all(c("pop", "sub") %in% names(Bxy_l))) {
         stop("Missing xB_l variables.")
     }
     Bxy_pop_l <- Bxy_l$pop
@@ -87,10 +289,9 @@ update_theta_l <- function(xB_l, Bxy_l, xKmat, kprec, para) {
 
 
 ## update delta_i
-update_delta_i <- function(Bmat_sub_i, xB_sub_i, kcontrib_pop_i, y_i, kprec, para) {
+update_delta_i <- function(Bmat_sub_i, xB_sub_i, y_star_i, kprec, para) {
     M_sub <- chol2inv(chol(xB_sub_i + kprec$sub / kprec$eps))
-    y_star <- y_i - kcontrib_pop_i
-    mu <- M_sub %*% crossprod(Bmat_sub_i, y_star)
+    mu <- M_sub %*% crossprod(Bmat_sub_i, y_star_i)
     sig <- M_sub / kprec$eps
     t(mvtnorm::rmvnorm(1, mu, sig))
 }
@@ -105,7 +306,7 @@ bayes_ridge_sub_v2 <- function(y, grp, Bmat, Kmat, dim_sub1, burn, size,
                                init = NULL, prior = NULL, verbose = TRUE) {
 
     grp <- check_grp(grp)
-    Bmat <- check_Bmat(Bmat)
+    Bmat <- check_Bmat(Bmat, Kmat)
     prior <- check_prior(prior, dim_sub1)
 
     ## some precalculation
@@ -129,22 +330,10 @@ bayes_ridge_sub_v2 <- function(y, grp, Bmat, Kmat, dim_sub1, burn, size,
     Bxy <- calc_Bxy(y, Bmat, idx_sub, para) # Bmat_pop x y, Bmat_sub x y
     xKmat <- crossprod(Kmat)
 
-    ## initialise the output list
-    samples <- list(population = array(NA, c(n_terms_sub, n_pops, size),
-                                       dimnames = list(NULL, levels(grp$pop))),
-                    subjects = array(NA, c(n_terms_sub, n_subs, size),
-                                     dimnames = list(NULL, levels(grp$sub))),
-                    precision = list(pop = rep(NA, size),
-                                     sub1 = array(NA, c(dim_sub1, dim_sub1, size)),
-                                     sub2 = rep(NA, size),
-                                     eps = rep(NA, size)),
-                    lp = rep(NA, size),
-                    ll = rep(NA, size))
-
     ## initialise theta with a penalised LS estimate, and delta with rnorms with
     ## small sd
     pls <- tcrossprod(solve(crossprod(Bmat$pop) + xKmat), Bmat$pop) %*% as.vector(y)
-    init <- initialise_with_pls(init, n_terms_sub, grp, pls)
+    init <- initialise_with_pls(init, para, grp, pls)
     kcoef <- list(pop = init$pop, sub = init$sub)
 
     ## initialise prediction contribution by population coefs and subjects deviations
@@ -157,11 +346,13 @@ bayes_ridge_sub_v2 <- function(y, grp, Bmat, Kmat, dim_sub1, burn, size,
         kcontrib_sub[idx_sub[[i]]] <- Bmat$sub[idx_sub[[i]], ] %*% kcoef$sub[, i]
     }
 
+    ## initialise the output list
+    samples <- initialise_samples(para, size, dim_sub1, grp)
+
     for (k in seq.int(-burn + 1, size)) {
         ## update precisions
         kresids <- y - kcontrib_pop - kcontrib_sub
         kprec <- update_prec(kcoef, kresids, para, prior)
-
         kprec$sub <- block_diag(kprec$sub1, diag(kprec$sub2, n_terms_sub - dim_sub1))
 
         ## update theta
@@ -169,21 +360,18 @@ bayes_ridge_sub_v2 <- function(y, grp, Bmat, Kmat, dim_sub1, burn, size,
             xB_l <- list(pop = xB$pop[, , subs_in_pop[[l]]],
                          sub = xB$sub[, , subs_in_pop[[l]]],
                          subpop = xB$subpop[, , subs_in_pop[[l]]])
-            Bxy_l <- list(pop = Bxy$pop[, , subs_in_pop[[l]]],
-                          sub = Bxy$sub[, , subs_in_pop[[l]]])
+            Bxy_l <- list(pop = Bxy$pop[, subs_in_pop[[l]]],
+                          sub = Bxy$sub[, subs_in_pop[[l]]])
             kcoef$pop[, l] <- update_theta_l(xB_l, Bxy_l, xKmat, kprec, para)
             kcontrib_pop[idx_pop[[l]]] <- Bmat$pop[idx_pop[[l]], ] %*% kcoef$pop[, l]
         }
 
-        ## fix from here on
-
         ## update delta
         for (i in levels(grp$sub)) {
-            M_sub <- chol2inv(chol(xB_sub[, , i] + kprec$sub / kprec$eps))
-            y_star <- y[idx_sub[[i]]] - kcontrib_pop[idx_sub[[i]]]
-            mu <- M_sub %*% crossprod(Bmat$sub[idx_sub[[i]], ], y_star)
-            sig <- M_sub / kprec$eps
-            kcoef$sub[, i] <- t(mvtnorm::rmvnorm(1, mu, sig))
+            Bmat_sub_i <- Bmat$sub[idx_sub[[i]], ]
+            xB_sub_i <- xB$sub[, , i]
+            y_star_i <- y[idx_sub[[i]]] - kcontrib_pop[idx_sub[[i]]]
+            kcoef$sub[, i] <- update_delta_i(Bmat_sub_i, xB_sub_i, y_star_i, kprec, para)
             kcontrib_sub[idx_sub[[i]]] <- Bmat$sub[idx_sub[[i]], ] %*% kcoef$sub[, i]
         }
 
@@ -192,6 +380,7 @@ bayes_ridge_sub_v2 <- function(y, grp, Bmat, Kmat, dim_sub1, burn, size,
             cat(k, " samples generated.\n")
         }
 
+        ## fix from here on
         ## store samples after burn-in iterations
         if (k > 0) {
             samples$precision$pop[k] <- kprec$pop
