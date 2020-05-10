efca1f71e04a04164f8310986b81754d36450a02
Kenyon Ng <work@kenyon.xyz>
2020-05-10 15:14:42 +0800

    Graphs v3 version
    1. A graphic function for fit_bs_spline_v3().

--- R/graphs.R
+++ R/graphs.R
@@ -28,6 +28,31 @@ extract_coef <- function(model, plot_type) {
   list(pop = coef_pop, sub = dev_sub)
 }
 
+## extract coefs from the model, where coef are theta and delta. 
+extract_coef_v3 <- function(model, plot_type) {
+
+  ## currently only works for plot_type == 'mean', throw error otherwise
+  stopifnot(plot_type == 'mean')
+
+  if (plot_type == 'mean') {
+    ## extract the posterior means
+    model$means$coef[c('theta', 'delta')]
+  ## } else if (plot_type == "map") {
+  ##   ## extract the map
+  ##   model_map <- get_max(model, "map")
+  ##   coef_pop <- model_map$population
+  ##   dev_sub <- model_map$subjects
+  ## } else if (plot_type == "mle") {
+  ##   ## extract the mle (maximum likelihood)
+  ##   model_mle <- get_max(model, "mle")
+  ##   coef_pop <- model_mle$population
+  ##   dev_sub <- model_mle$subjects
+  } else {
+    stop("Unknown plotting type.")
+  }
+  ## list(pop = coef_pop, sub = dev_sub)
+}
+
 ## calculate model matrix given x and a basis information
 get_model_mat <- function(x, binfo) {
 
@@ -158,6 +183,74 @@ plot_spline <- function(model, limits = NULL, plot_which = NULL, plot_type = "me
   invisible(ggls)
 }
 
+
+
+## Plot the population and subject curves (suitable for models with multiple
+## populations) 'plot_which' takes a vector of names (for models with multiple
+## populations) type can only be "mean" at the momment. This will work for model
+## where the coefs of the splines are named as 'theta' and 'delta' rather than
+## 'population' and 'subject'. Works for object from fit_bs_spline_v3()
+
+## If shade is true, thin the population samples by 10 and plot the thinned
+## samples.
+
+## Requires: model$basis$knots (including extrema), model$basis$type,
+##   model$basis$degree, model$info$lvl_pop, model$data, model$mean,
+##   model$samples$coef$theta (if shade is true)
+
+#' @importFrom magrittr %>%
+#' @importFrom ggplot2 aes_ geom_point geom_line geom_ribbon
+plot_spline_v3 <- function(model, limits = NULL, plot_which = NULL, plot_type = "mean",
+                        shade = FALSE, silent = FALSE) {
+
+  data <- model$data
+  subs_in_pop <- tapply(as.character(data$sub), data$pop, unique, simplify = FALSE)
+
+  limits <- get_limits(limits, data$x)                    # limit of x
+  plot_x <- get_plot_x(model, limits)                     # range of x to plot
+  model_mat_pop <- get_model_mat(plot_x, model$basis$pop) # model matrix for the pop curves
+  model_mat_sub <- get_model_mat(plot_x, model$basis$sub) # model matrix for the sub curves
+
+  plot_which <- get_plot_which(plot_which, levels(data$pop)) # plot which populations?
+  coefs <- extract_coef_v3(model, plot_type)           # extract coefs and devs
+
+  
+  ggls <- list()
+  for (l in plot_which) {
+    ## shading
+    if (shade) {
+      plotdat_thin <- get_plotdat_thin(model$samples$coef$theta[, l, ],
+                                       model_mat_pop, plot_x)
+      ggls[[l]]$rib90 <- geom_ribbon(aes_(~x, ymin = ~`5%`, ymax = ~`95%`),
+                                     plotdat_thin, fill = "grey85", alpha = 0.7)
+      ggls[[l]]$rib50 <- geom_ribbon(aes_(~x, ymin = ~`25%`, ymax = ~`75%`),
+                                     plotdat_thin, fill = "grey65", alpha = 0.5)
+    }
+
+    ## data
+    ggls[[l]]$data <- geom_point(aes_(~x, ~y, col = ~sub), data[data$pop == l, ])
+
+    ## curves
+    subs_in_l <- subs_in_pop[[l]]
+    plot_y_pop <- as.numeric(model_mat_pop %*% coefs$theta[, l])
+    plot_y_sub <- plot_y_pop + (model_mat_sub %*% coefs$delta[, subs_in_l])
+    plotdat_pop <- tibble::tibble(x = plot_x, y = plot_y_pop)
+    plotdat_sub <- tibble::as_tibble(plot_y_sub) %>%
+      dplyr::mutate(x = plot_x) %>%
+      tidyr::gather("sub", "y", -"x")
+    ggls[[l]]$sub <- geom_line(aes_(~x, ~y, col = ~sub, group = ~sub), plotdat_sub)
+    ggls[[l]]$pop <- geom_line(aes_(~x, ~y), data = plotdat_pop)
+
+    if (!silent) {
+      print(ggplot2::ggplot() + ggls[[l]]$rib90 + ggls[[l]]$rib50 +
+              ggls[[l]]$data + ggls[[l]]$sub + ggls[[l]]$pop +
+              ggplot2::theme_bw() +
+              ggplot2::theme(legend.position="none") +
+              ggplot2::ggtitle(paste("Population :", l)))
+    }
+  }
+  invisible(ggls)
+}
 
 
 
