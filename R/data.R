71ffc45f278ac6169f9fa076562d4226286d6909
Kenyon Ng <work@kenyon.xyz>
2020-05-14 16:13:25 +0800

    Add simdata3, testing will models with fixed effects only
    1. Add simdata3 (with an 'effect' variable).
    2. Add tests for models with fixed effects.

--- R/data.R
+++ R/data.R
@@ -28,3 +28,40 @@ get_simdata2 <- function(seed = 1) {
 
   data.frame(y = y, x = rep(x, n_subs),  sub = grp$sub, pop = grp$pop, truth = f + g)
 }
+
+
+get_simdata3 <- function(seed = 1) {
+  ## a quadratic spline with 3 interior knots
+  set.seed(seed)
+  x <- 1:20 + rnorm(20, sd = 0.1) # 20 samples for each subject
+  K <- 3
+  deg <- 2
+  n_pops <- 3
+  n_subs <- 10
+  ## 1st group = 3 subs, 2nd group = 3 subs, 3rd group = 4 subs, 
+  n_subs_in_pop <- list('1' = 3, '2' = 3, '3' = 4)
+  grp <- list(pop = unlist(purrr::imap(n_subs_in_pop, ~rep(.y, .x * length(x))), F, F),
+              sub = as.character(rep(1:10, each = length(x))))
+
+  Bmat <- get_design_bs(x, K, deg)$design
+  ## sub 1, 3, 5, 7, 9 has one effect, the rest another
+  effect <- as.factor(as.numeric(grp$sub) %% 2 == 0)
+  
+  dim_theta <- K + deg + 1
+  dim_delta <- K + deg + 1
+
+  ## theta: 1:dim_theta + std Gaussian
+  ## delta: std Gaussian
+  theta <- matrix(1:dim_theta + rnorm(n_pops * dim_theta), dim_theta, n_pops,
+                  dimnames = list(NULL, seq(1, n_pops)))
+  delta <- matrix(rnorm(n_subs * dim_delta) * 0.5, dim_delta, n_subs)
+  
+  f <- unlist(purrr::imap(n_subs_in_pop, ~rep(Bmat %*% theta[, .y], .x)), F, F)
+  g <- c(Bmat %*% delta)
+  y <- f + g + rnorm(length(f), sd = 0.2)
+
+  data.frame(y = y, x = rep(x, n_subs),  sub = grp$sub, pop = grp$pop, truth = f + g,
+             effect = effect)
+}
+
+
--- tests/test-fit-ridge.R
+++ tests/test-fit-ridge.R
@@ -8,8 +8,12 @@ burn <- 0
 ## init <- list(pop = get_pls(data$y, Bmat$pop, Kmat) + rnorm(NCOL(Bmat$pop), sd = 100))
 set.seed(1)
 fm1 <- fit_bs_splines(simdata, K, deg, size, burn, ridge = FALSE, init = NULL, prior = NULL)
+
+load_all()
 set.seed(1)
-fm2 <- fit_bs_splines_v3(simdata, K, deg, size, burn, ridge = FALSE, init = NULL, prior = NULL)
+fm2 <- fit_bs_splines_v3(simdata, K = K, deg = deg,
+                         size = size, burn = burn, ridge = FALSE, init = NULL, prior = NULL)
+plot_spline_v3(fm2)
 
 
 ## example with multiple population and subject curves
@@ -19,5 +23,19 @@ fm3 <- fit_bs_splines(simdata2, K, deg, size, burn, ridge = FALSE, init = NULL, 
 
 load_all()
 set.seed(1)
-fm4 <- fit_bs_splines_v3(simdata2, K, deg, size, burn, ridge = FALSE, init = NULL, prior = NULL)
+fm4 <- fit_bs_splines_v3(simdata2, K = K, deg = deg, Xmat = NULL, ranef = NULL,
+                         size = size, burn = burn, ridge = FALSE, init = NULL, prior = NULL)
+plot_spline(fm3)
+
+
+## example with multiple population, subject curves, and fixed effects
+load_all()
+simdata3 <- get_simdata3()
+set.seed(1)
+Xmat <- model.matrix(~effect - 1, simdata3)
+ranef <- NULL # all betas are fixed
+fm4 <- fit_bs_splines_v3(simdata3, K = K, deg = deg, Xmat = Xmat, ranef = ranef,
+                         size = size, burn = burn, ridge = FALSE, init = NULL, prior = NULL)
+plot(fm4$samples$coef$theta[1, 1, ])
+plot_spline(fm3)
 
